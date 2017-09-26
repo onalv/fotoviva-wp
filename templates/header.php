9798eb85b10a44c5b3913d5ae3c825f3e1f76459
onalv <omarnavaalvarez@gmail.com>
2017-09-25 22:27:45 -0500

    Added nav walker for b4 and images

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,24 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo-black.png" width="160px" alt="Logo Fotoviva">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <?php
+        wp_nav_menu( array(
+                'theme_location'    => 'primary_navigation',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'navbarSupportedContent',
+                'menu_class'        => 'navbar-nav ml-auto',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker())
+        );
+        ?>
+
+    </div> <!--container-->
+</nav>
