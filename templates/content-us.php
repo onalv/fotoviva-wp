adc7150b3f4fcfa5d36eda8d0c2d9e2d07dae760
onalv <omarnavaalvarez@gmail.com>
2017-10-09 08:50:37 -0500

    style to us

--- templates/content-us.php
+++ templates/content-us.php
@@ -22,57 +22,76 @@
 <section id="container-nuestro-centro">
     <div class="container">
         <div class="row">
-            <div class="col-6">
+            <div class="col-6 div-text">
                 <h1>Nuestro Centro</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
             </div>
-            <div class="col-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dicta hic libero nostrum similique? Dolores, earum eos esse fuga impedit neque perspiciatis quasi? Beatae commodi dolor ducimus inventore nesciunt, quam?</p>
+            <div class="col-6 div-img">
+                <img src="<?php echo get_template_directory_uri(); ?>/dist/images/estudio.jpg" alt="Foto Estudio" class="img-fluid">
             </div>
         </div>
     </div>
 </section>
 <section id="container-colaboradores-nacionales">
     <div class="container">
-        <h1 class="text-center">Colaboradores Nacional</h1>
+        <h1 class="text-center">Colaboradores Nacionales</h1>
         <div class="row">
             <div class="owl-two owl-carousel owl-theme">
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Juan Valdez</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/359.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Omar Mora</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Andrea Garza</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/157.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Sebastián Portillo</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Joaquin Sabina</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/258.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Andrea Garza</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Pedro Infante</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/359.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Sebastián Portillo</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Pablo Marmol</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/157.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Sebastián Portillo</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Juan Valdez</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/463.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Paulina Mejía</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
             </div>
@@ -86,38 +105,56 @@
             <div class="owl-three owl-carousel owl-theme">
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Juan Valdez</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/463.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Sebastián Portillo</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Andrea Garza</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/157.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Steve McCurry</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Joaquin Sabina</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/258.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Jessica Simpson</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Pedro Infante</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/359.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Sebastián Portillo</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Pablo Marmol</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/463.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Britney Kaylor</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
                 <div class="owl-item">
                     <figure>
-                        <img src="https://placeimg.com/320/480/people" alt="Colaborador 1" class="img-fluid">
-                        <figcaption>Juan Valdez</figcaption>
+                        <img src="http://electricblaze.com/unicore/page-builder/assets/images/258.jpg" alt="Colaborador 1" class="img-fluid">
+                        <figcaption>
+                            <h4>Jasmine Hunterton</h4>
+                            <p>Sed ut unde omnis iste natus error sit accus antium dolor emque laud antium.</p>
+                        </figcaption>
                     </figure>
                 </div>
             </div>
