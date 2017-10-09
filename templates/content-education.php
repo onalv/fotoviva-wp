d161b0294059e1c00326b984862b866c164c6f3a
onalv <omarnavaalvarez@gmail.com>
2017-10-08 21:52:59 -0500

    .

--- templates/content-education.php
+++ templates/content-education.php
@@ -10,40 +10,40 @@
         </div>
         <div class="row grid" id="isotope-container">
             <div class="col-4 element-item talleres" data-category="talleres">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/900/900/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item cursos" data-category="cursos">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item talleres" data-category="talleres">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/700/700/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item masterclass" data-category="masterclass">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item cursos" data-category="cursos">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/600/600/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item cursos" data-category="cursos">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/900/900/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/750/750/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/850/850/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item talleres" data-category=talleres">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/720/720/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item masterclass" data-category="masterclass">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/760/760/any" alt="Curso" class="img-fluid">
             </div>
             <div class="col-4 element-item masterclass" data-category="masterclass">
-                <img src="https://placeimg.com/320/320/any" alt="Curso" class="img-fluid">
+                <img src="https://placeimg.com/860/860/any" alt="Curso" class="img-fluid">
             </div>
 
 
