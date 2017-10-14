98a85c4327943bc39eed67d96ba8588226ef7756
onalv <omarnavaalvarez@gmail.com>
2017-10-14 01:20:28 -0500

    margin and padding to zero for divs

--- templates/content-exhibitions.php
+++ templates/content-exhibitions.php
@@ -1,40 +1,60 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-4">
-            <a href="https://placeimg.com/1400/1400/any" data-lightbox="page-exhibition-1">
-                <img src="https://placeimg.com/900/900/any" alt="Exhibition" class="img-fluid" />
-            </a>
-            <a href="https://placeimg.com/1140/1440/any" data-lightbox="page-exhibition-1"></a>
-            <a href="https://placeimg.com/1160/1140/any" data-lightbox="page-exhibition-1"></a>
-            <a href="https://placeimg.com/1600/1200/any" data-lightbox="page-exhibition-1"></a>
-            <a href="https://placeimg.com/1400/1400/any" data-lightbox="page-exhibition-1"></a>
-        </div>
-        <div class="col-4">
-            <a href="https://placeimg.com/1400/1400/any" data-lightbox="page-exhibition-2">
-                <img src="https://placeimg.com/920/920/any" alt="Exhibition" class="img-fluid" />
-            </a>
-            <a href="https://placeimg.com/1200/1440/any" data-lightbox="page-exhibition-2"></a>
-            <a href="https://placeimg.com/1300/1140/any" data-lightbox="page-exhibition-2"></a>
-            <a href="https://placeimg.com/1400/1200/any" data-lightbox="page-exhibition-2"></a>
-            <a href="https://placeimg.com/1400/1100/any" data-lightbox="page-exhibition-2"></a>
-        </div>
-        <div class="col-4">
-            <a href="https://placeimg.com/1200/1000/any" data-lightbox="page-exhibition-3">
-                <img src="https://placeimg.com/960/960/any" alt="Exhibition" class="img-fluid" />
-            </a>
-            <a href="https://placeimg.com/1240/1440/any" data-lightbox="page-exhibition-3"></a>
-            <a href="https://placeimg.com/1300/1140/any" data-lightbox="page-exhibition-3"></a>
-            <a href="https://placeimg.com/1500/1200/any" data-lightbox="page-exhibition-3"></a>
-            <a href="https://placeimg.com/1250/1400/any" data-lightbox="page-exhibition-3"></a>
-        </div>
-        <div class="col-4">
-            <a href="https://placeimg.com/1200/1200/any" data-lightbox="page-exhibition-4">
-                <img src="https://placeimg.com/1000/1000/any" alt="Exhibition" class="img-fluid" />
-            </a>
-            <a href="https://placeimg.com/1040/1440/any" data-lightbox="page-exhibition-4"></a>
-            <a href="https://placeimg.com/1360/1140/any" data-lightbox="page-exhibition-4"></a>
-            <a href="https://placeimg.com/1500/1200/any" data-lightbox="page-exhibition-4"></a>
-            <a href="https://placeimg.com/1300/1400/any" data-lightbox="page-exhibition-4"></a>
+<section id="container-exhibitions">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4">
+                <a href="https://placeimg.com/1400/1400/any" data-lightbox="page-exhibition-1">
+                    <img src="https://placeimg.com/900/900/any" alt="Exhibition" class="img-fluid" />
+                </a>
+                <a href="https://placeimg.com/1140/1440/any" data-lightbox="page-exhibition-1"></a>
+                <a href="https://placeimg.com/1160/1140/any" data-lightbox="page-exhibition-1"></a>
+                <a href="https://placeimg.com/1600/1200/any" data-lightbox="page-exhibition-1"></a>
+                <a href="https://placeimg.com/1400/1400/any" data-lightbox="page-exhibition-1"></a>
+            </div>
+            <div class="col-4">
+                <a href="https://placeimg.com/1400/1400/any" data-lightbox="page-exhibition-2">
+                    <img src="https://placeimg.com/920/920/any" alt="Exhibition" class="img-fluid" />
+                </a>
+                <a href="https://placeimg.com/1200/1440/any" data-lightbox="page-exhibition-2"></a>
+                <a href="https://placeimg.com/1300/1140/any" data-lightbox="page-exhibition-2"></a>
+                <a href="https://placeimg.com/1400/1200/any" data-lightbox="page-exhibition-2"></a>
+                <a href="https://placeimg.com/1400/1100/any" data-lightbox="page-exhibition-2"></a>
+            </div>
+            <div class="col-4">
+                <a href="https://placeimg.com/1200/1000/any" data-lightbox="page-exhibition-3">
+                    <img src="https://placeimg.com/960/960/any" alt="Exhibition" class="img-fluid" />
+                </a>
+                <a href="https://placeimg.com/1240/1440/any" data-lightbox="page-exhibition-3"></a>
+                <a href="https://placeimg.com/1300/1140/any" data-lightbox="page-exhibition-3"></a>
+                <a href="https://placeimg.com/1500/1200/any" data-lightbox="page-exhibition-3"></a>
+                <a href="https://placeimg.com/1250/1400/any" data-lightbox="page-exhibition-3"></a>
+            </div>
+            <div class="col-4">
+                <a href="https://placeimg.com/1200/1200/any" data-lightbox="page-exhibition-4">
+                    <img src="https://placeimg.com/1000/1000/any" alt="Exhibition" class="img-fluid" />
+                </a>
+                <a href="https://placeimg.com/1040/1440/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1360/1140/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1500/1200/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1300/1400/any" data-lightbox="page-exhibition-4"></a>
+            </div>
+            <div class="col-4">
+                <a href="https://placeimg.com/1300/1300/any" data-lightbox="page-exhibition-4">
+                    <img src="https://placeimg.com/800/800/any" alt="Exhibition" class="img-fluid" />
+                </a>
+                <a href="https://placeimg.com/1040/1440/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1360/1140/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1500/1200/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1300/1400/any" data-lightbox="page-exhibition-4"></a>
+            </div>
+            <div class="col-4">
+                <a href="https://placeimg.com/1100/1100/any" data-lightbox="page-exhibition-4">
+                    <img src="https://placeimg.com/900/900/any" alt="Exhibition" class="img-fluid" />
+                </a>
+                <a href="https://placeimg.com/1040/1440/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1360/1140/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1500/1200/any" data-lightbox="page-exhibition-4"></a>
+                <a href="https://placeimg.com/1300/1400/any" data-lightbox="page-exhibition-4"></a>
+            </div>
         </div>
     </div>
-</div>
+</section>
