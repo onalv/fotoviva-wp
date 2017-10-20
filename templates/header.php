5401ee9df0a91cd92b837546accf35f7c0719cd5
onalv <omarnavaalvarez@gmail.com>
2017-10-19 20:52:38 -0500

    init upper fixed menu

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container">
+
         <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo-black.png" alt="Logo Fotoviva" class="img-logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <ul class="navbar-nav" id="navbar-fixed-top">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
 
         <?php
         wp_nav_menu( array(
