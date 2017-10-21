f461759ce1e20fbc6bc0a6828d745a1114c010bd
onalv <omarnavaalvarez@gmail.com>
2017-10-20 22:37:39 -0500

    finished static menu

--- templates/header.php
+++ templates/header.php
@@ -8,11 +8,15 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <ul class="navbar-nav" id="navbar-fixed-top">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li style="background-color: transparent;">
+                <a href="https://www.instagram.com/fotoviva_oficial/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/social-1-gray.png" alt="Instagram Fotoviva"></a></li>
+            <li style="background-color: transparent;">
+                <a href="https://www.facebook.com/fotovivamx/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/social-2-gray.png" alt="Facebook Fotoviva"></a></li>
+            <li class="nav-item">
+                <a class="nav-link" href="tel:4564567789">Tel: (456) 4567-789 <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="mailto:contacto@fotoviva.mx">contacto@fotoviva.mx</a>
             </li>
         </ul>
 
