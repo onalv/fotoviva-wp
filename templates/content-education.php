612c341838a937f83996b6624f6f969cf7f43b1b
onalv <omarnavaalvarez@gmail.com>
2017-10-22 20:01:30 -0500

    Added WP QUERY to courses in courses page

--- templates/content-education.php
+++ templates/content-education.php
@@ -1,137 +1,66 @@
+<?php $counter = 1; ?>
+
 <section id="container-page-education">
     <div class="container">
         <div id="filters" class="row button-group">
             <div class="col"><button class="btn btn-block btn-dark is-checked active" data-filter="*">Todos</button></div>
-            <div class="col"><button class="btn btn-block btn-dark" data-filter=".especializaciones">Especializaciones</button></div>
-            <div class="col"><button class="btn btn-block btn-dark" data-filter=".seminarios">Seminarios</button></div>
-            <div class="col"><button class="btn btn-block btn-dark" data-filter=".talleres">Talleres</button></div>
-            <div class="col"><button class="btn btn-block btn-dark" data-filter=".clasesAbiertas">Clases abiertas</button></div>
+            <div class="col"><button class="btn btn-block btn-dark" data-filter=".especializacion">Especializaciones</button></div>
+            <div class="col"><button class="btn btn-block btn-dark" data-filter=".seminario">Seminarios</button></div>
+            <div class="col"><button class="btn btn-block btn-dark" data-filter=".taller">Talleres</button></div>
+            <div class="col"><button class="btn btn-block btn-dark" data-filter=".clase">Clases abiertas</button></div>
             <div class="col"><button class="btn btn-block btn-dark" data-filter=".masterclass">Masterclass</button></div>
         </div>
         <div class="row grid" id="isotope-container">
 
-            <div class="col-4 element-item seminarios" data-category="seminarios">
-                <img src="https://placeimg.com/700/700/any" alt="Curso" class="img-fluid">
-                <h2>Fotografía de moda</h2>
+            <?php $the_query = new WP_Query( array('post_type' => 'cursos', 'posts_per_page' => 100) ); ?>
+            <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
 
-                <button class="btn btn-light btn-course" data-toggle="modal" data-target=".bd-example-modal-lg2">+Info</button>
-                <!-- Large modal -->
-                <div class="modal fade bd-example-modal-lg2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h3 class="modal-title">Fotografía de moda</h3>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="container-fluid">
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <img src="https://placeimg.com/700/700/any" alt="Curso" class="img-fluid">
-                                        </div>
-                                        <div class="col-6">
-                                            <ul>
-                                                <li>Price: $1,600</li>
-                                                <li>Duración de curso: 6 semanas</li>
-                                                <li>Nivel: Intermedio</li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-12 mt-3">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi dolorum eligendi et, nam perferendis quaerat quis reprehenderit sint. Dolore earum est facilis, nemo perferendis quas ratione tenetur veniam.</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi dolorum eligendi et, nam perferendis quaerat quis reprehenderit sint. Dolore earum est facilis, nemo perferendis quas ratione tenetur veniam.</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi dolorum eligendi et, nam perferendis quaerat quis reprehenderit sint. Dolore earum est facilis, nemo perferendis quas ratione tenetur veniam.</p>
+                <div class="col-4 element-item <?php the_field('tipo'); ?>" data-category="<?php the_field('tipo'); ?>">
+                    <?php the_post_thumbnail( array(480, 360), array( 'class' => 'img-fluid' ) ); ?>
+                    <h2><?php the_field('tipo'); ?> de <span><?php the_title(); ?></span></h2>
+
+                    <button class="btn btn-light btn-course" data-toggle="modal" data-target=".modalCursos-<?php echo $counter; ?>">+Info</button>
+                    <!-- Large modal -->
+                    <div class="modal fade modalCursos-<?php echo $counter; ?>" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h3 class="modal-title"><?php the_field('tipo'); ?> de <span><?php the_title(); ?></span></h3>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <?php the_post_thumbnail( array(480, 360), array( 'class' => 'img-fluid' ) ); ?>
+                                            </div>
+                                            <div class="col-6">
+                                                <ul>
+                                                    <li>Precio: $<?php the_field('precio'); ?></li>
+                                                    <li>Duración de curso: 6 semanas</li>
+                                                    <li>Nivel: Intermedio</li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-12 mt-3">
+                                                <?php the_content(); ?>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-success">Comprar</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-success">Comprar</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
+            <?php $counter++; ?>
+            <?php endwhile; wp_reset_query(); ?>
 
-            <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
-                <h2>Fotografía avanzada</h2>
-
-                <button class="btn btn-light btn-course" data-toggle="modal" data-target=".bd-example-modal-lg">+Info</button>
-                <!-- Large modal -->
-                <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h3 class="modal-title">Fotografía Avanzada</h3>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="container-fluid">
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
-                                        </div>
-                                        <div class="col-6">
-                                            <ul>
-                                                <li>Price: $1,600</li>
-                                                <li>Duración de curso: 6 semanas</li>
-                                                <li>Nivel: Intermedio</li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-12 mt-3">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi dolorum eligendi et, nam perferendis quaerat quis reprehenderit sint. Dolore earum est facilis, nemo perferendis quas ratione tenetur veniam.</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi dolorum eligendi et, nam perferendis quaerat quis reprehenderit sint. Dolore earum est facilis, nemo perferendis quas ratione tenetur veniam.</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi dolorum eligendi et, nam perferendis quaerat quis reprehenderit sint. Dolore earum est facilis, nemo perferendis quas ratione tenetur veniam.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-success">Comprar</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-4 element-item talleres" data-category="talleres">
-                <img src="https://placeimg.com/700/700/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item masterclass" data-category="masterclass">
-                <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/600/600/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/900/900/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item especializaciones" data-category="especializaciones">
-                <img src="https://placeimg.com/750/750/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item seminarios" data-category="seminarios">
-                <img src="https://placeimg.com/850/850/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item talleres" data-category=talleres">
-                <img src="https://placeimg.com/720/720/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item masterclass" data-category="masterclass">
-                <img src="https://placeimg.com/800/800/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item clasesAbiertas" data-category="clasesAbiertas">
-                <img src="https://placeimg.com/760/760/any" alt="Curso" class="img-fluid">
-            </div>
-            <div class="col-4 element-item masterclass" data-category="masterclass">
-                <img src="https://placeimg.com/860/860/any" alt="Curso" class="img-fluid">
-            </div>
         </div>
     </div>
 </section>
