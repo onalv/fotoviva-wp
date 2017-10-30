4764db35ebc6ab392b3451440165aa1199df8e6e
onalv <omarnavaalvarez@gmail.com>
2017-10-30 15:19:15 -0600

    display none upper fixed menu

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <ul class="navbar-nav" id="navbar-fixed-top">
+        <ul class="navbar-nav d-none d-lg-flex" id="navbar-fixed-top">
             <li class="nav-item">
                 <a class="nav-link" href="tel:4564567789">Tel: (456) 4567-789 <span class="sr-only">(current)</span></a>
             </li>
@@ -18,7 +18,6 @@
                 <a href="https://www.instagram.com/fotoviva_oficial/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/social-1-gray.png" alt="Instagram Fotoviva"></a></li>
             <li style="background-color: transparent;">
                 <a href="https://www.facebook.com/fotovivamx/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/social-2-gray.png" alt="Facebook Fotoviva"></a></li>
-
         </ul>
 
         <?php
