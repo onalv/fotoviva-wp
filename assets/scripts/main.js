394d5233bf97ef99dc21546cc3e9ef735aa600b5
onalv <omarnavaalvarez@gmail.com>
2018-01-15 01:08:58 -0600

    minor === change

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -35,7 +35,7 @@
         var topOffset = 55;
 
         $('a[href*="#"]').click(function() {
-          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
               var target = $(this.hash);
               target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
               if (target.length) {
