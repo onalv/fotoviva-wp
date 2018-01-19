e8f7e7bc83980a18c3db4f43834866ab36f64067
onalv <omarnavaalvarez@gmail.com>
2018-01-19 00:27:09 -0600

    added cine template and style

--- templates/content-cine.php
+++ templates/content-cine.php
@@ -1,3 +1,68 @@
+<div id="video">
+    <video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" playsinline autoplay muted loop>
+        <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+        <source src="http://thenewcode.com/assets/videos/polina.webm" type="video/webm">
+        <source src="http://thenewcode.com/assets/videos/polina.mp4" type="video/mp4">
+    </video>
+</div>
+
 <section id="cine">
-    <h1>hola cine</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Master en Cinematografía</h1>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur earum eligendi fuga perferendis qui quis rerum. Eum, natus sed? At blanditiis et fuga fugit molestiae quidem, sunt suscipit velit veritatis.</span><span>Deserunt eius, error minima possimus quasi velit voluptate? A debitis doloribus error quasi qui sapiente veritatis vero, voluptas! Aliquam commodi debitis deleniti doloribus exercitationem, inventore numquam omnis quam reprehenderit vel!</span><span>Ab atque doloribus dolorum, ducimus et labore porro quae, quam qui quis sint suscipit tenetur. Adipisci doloribus, eligendi enim illo iure labore nemo odio sed sunt, tempora temporibus ullam voluptatum!</span><span>Accusamus accusantium architecto consectetur cum debitis delectus dolore, ex hic id ipsum iure laborum modi necessitatibus nisi nostrum odio omnis quam qui quia reiciendis sed sunt temporibus totam vitae voluptatem!</span><span>Labore, nisi, similique. Ab adipisci cum, cupiditate deserunt facere incidunt ipsum modi nihil optio perspiciatis placeat, possimus quas quasi quo quos reiciendis similique. Debitis deleniti dolores ipsum possimus similique voluptatem!</span></p>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At distinctio dolor ducimus ea harum magni maiores, natus non nostrum quaerat quas quod quos ratione reprehenderit sit tempore velit vitae voluptatem.</span><span>Consectetur fuga illo nulla, numquam quo suscipit tempora. Accusamus aperiam assumenda commodi consequuntur id inventore minima! Assumenda cum illum maxime odio officiis optio quas, recusandae veritatis? Amet eos labore soluta!</span><span>Atque impedit laboriosam quaerat! Alias at dignissimos eum facere fugiat id ipsum itaque iusto laudantium molestiae mollitia nam non, porro, quis repellat suscipit, voluptatibus! Assumenda atque beatae consequuntur eum officia.</span></p>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, corporis delectus doloribus error fugit in incidunt iusto labore necessitatibus non nulla optio qui quidem tempora temporibus veritatis. Quibusdam, totam!</span><span>Accusamus debitis dicta earum, eius eligendi et expedita explicabo in labore magni nihil non quod ratione repellat, rerum sint unde voluptatibus voluptatum. Aliquid explicabo maiores quia voluptas. At, nemo, repudiandae.</span><span>Amet consectetur facere ipsum nam. Eligendi enim provident ullam. Accusamus alias animi, autem commodi eveniet ex hic ipsa minima nostrum numquam odio porro quis quod ratione sequi tempore vero voluptas?</span><span>Beatae blanditiis, non optio quibusdam repellendus voluptate! Animi asperiores debitis dicta dolorum ducimus ea earum et in incidunt ipsa magni modi officia placeat quaerat quam, rem suscipit vel velit voluptatem.</span><span>At aut consequatur consequuntur cum debitis distinctio, dolores enim error iste itaque maiores necessitatibus, numquam qui quidem repellat! Aliquam at consequatur corporis dicta id illum ipsam labore molestiae numquam quos?</span><span>Commodi eveniet excepturi ipsa ipsum itaque maxime minima molestias, mollitia nemo, sequi sit veritatis. Ad amet corporis debitis expedita hic ipsa non omnis perferendis quae repudiandae soluta unde, velit voluptatum.</span><span>At earum, et excepturi nostrum omnis quaerat sit tenetur! Architecto beatae ex harum hic illo laudantium magni nihil, nisi omnis porro quas quasi ratione suscipit vel voluptas voluptates, voluptatibus! Doloremque?</span></p>
+            </div>
+        </div>
+        <h3>Áreas a cursar</h3>
+        <div class="row">
+            <div class="col-8">
+                <h4>Módulo 1</h4>
+                <ul>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                </ul>
+                <h4>Módulo 2</h4>
+                <ul>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                </ul>
+                <h4>Módulo 3</h4>
+                <ul>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <p>
+                    <span>INSCRIPCIÓN $4,400</span><br>
+                    <span>COSTO POR MÓDULO</span><br>
+                    <span>DESCUENTO MASTER</span><br>
+                    <span>CLASES OPCIONALES</span><br>
+                </p>
+                <p>
+                    <span>REQUISITOS DE INSCRIPCIÓN:</span>
+                </p>
+                <p>
+                    <span>lorem lorem</span><br>
+                    <span>lorem lorem</span><br>
+                    <span>lorem lorem</span><br>
+                    <span>lorem lorem</span><br>
+                    <span>lorem lorem</span><br>
+                    <span>lorem lorem</span><br>
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
