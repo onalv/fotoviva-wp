9671d10604b227f8bb4f7cb7af84ee78eb161662
onalv <omarnavaalvarez@gmail.com>
2018-01-19 22:52:39 -0600

    added text to cine page

--- templates/content-cine.php
+++ templates/content-cine.php
@@ -10,10 +10,17 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Master en Cinematografía</h1>
-                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur earum eligendi fuga perferendis qui quis rerum. Eum, natus sed? At blanditiis et fuga fugit molestiae quidem, sunt suscipit velit veritatis.</span><span>Deserunt eius, error minima possimus quasi velit voluptate? A debitis doloribus error quasi qui sapiente veritatis vero, voluptas! Aliquam commodi debitis deleniti doloribus exercitationem, inventore numquam omnis quam reprehenderit vel!</span><span>Ab atque doloribus dolorum, ducimus et labore porro quae, quam qui quis sint suscipit tenetur. Adipisci doloribus, eligendi enim illo iure labore nemo odio sed sunt, tempora temporibus ullam voluptatum!</span><span>Accusamus accusantium architecto consectetur cum debitis delectus dolore, ex hic id ipsum iure laborum modi necessitatibus nisi nostrum odio omnis quam qui quia reiciendis sed sunt temporibus totam vitae voluptatem!</span><span>Labore, nisi, similique. Ab adipisci cum, cupiditate deserunt facere incidunt ipsum modi nihil optio perspiciatis placeat, possimus quas quasi quo quos reiciendis similique. Debitis deleniti dolores ipsum possimus similique voluptatem!</span></p>
-                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At distinctio dolor ducimus ea harum magni maiores, natus non nostrum quaerat quas quod quos ratione reprehenderit sit tempore velit vitae voluptatem.</span><span>Consectetur fuga illo nulla, numquam quo suscipit tempora. Accusamus aperiam assumenda commodi consequuntur id inventore minima! Assumenda cum illum maxime odio officiis optio quas, recusandae veritatis? Amet eos labore soluta!</span><span>Atque impedit laboriosam quaerat! Alias at dignissimos eum facere fugiat id ipsum itaque iusto laudantium molestiae mollitia nam non, porro, quis repellat suscipit, voluptatibus! Assumenda atque beatae consequuntur eum officia.</span></p>
-                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, corporis delectus doloribus error fugit in incidunt iusto labore necessitatibus non nulla optio qui quidem tempora temporibus veritatis. Quibusdam, totam!</span><span>Accusamus debitis dicta earum, eius eligendi et expedita explicabo in labore magni nihil non quod ratione repellat, rerum sint unde voluptatibus voluptatum. Aliquid explicabo maiores quia voluptas. At, nemo, repudiandae.</span><span>Amet consectetur facere ipsum nam. Eligendi enim provident ullam. Accusamus alias animi, autem commodi eveniet ex hic ipsa minima nostrum numquam odio porro quis quod ratione sequi tempore vero voluptas?</span><span>Beatae blanditiis, non optio quibusdam repellendus voluptate! Animi asperiores debitis dicta dolorum ducimus ea earum et in incidunt ipsa magni modi officia placeat quaerat quam, rem suscipit vel velit voluptatem.</span><span>At aut consequatur consequuntur cum debitis distinctio, dolores enim error iste itaque maiores necessitatibus, numquam qui quidem repellat! Aliquam at consequatur corporis dicta id illum ipsam labore molestiae numquam quos?</span><span>Commodi eveniet excepturi ipsa ipsum itaque maxime minima molestias, mollitia nemo, sequi sit veritatis. Ad amet corporis debitis expedita hic ipsa non omnis perferendis quae repudiandae soluta unde, velit voluptatum.</span><span>At earum, et excepturi nostrum omnis quaerat sit tenetur! Architecto beatae ex harum hic illo laudantium magni nihil, nisi omnis porro quas quasi ratione suscipit vel voluptas voluptates, voluptatibus! Doloremque?</span></p>
+                <h1>Diplomado en Artes y Ciencias Cinematográficas</h1>
+                <p>
+                    Dirigido a jóvenes y adultos que aspiran a convertirse en creadores de cine digital, por medio de una intensa formación en los fundamentos, procesos y canales necesarios para producir contenidos audiovisuales, que se adapten a las necesidades y circunstancias socioculturales, artísticas, publicitarias o conceptuales de la realidad actual dentro de la industria cinematográfica.
+                    Nuestro master consta de 720 horas teórico prácticas, distribuidas durante 9 meses, en 3 niveles especializados de 240 horas cada uno; ellos abarcan en un nivel exhaustivo las etapas de preproducción, producción y postproducción.
+                </p>
+                <p>
+                    El cine es un arte colectivo y este master se ha diseñado desde las principales áreas de la cinematografía, para ofrecer así una visión global y especializada de la estructura requerida en un proyecto de cine, que encause a los alumnos a que descubran su perfil y determinen su lugar dentro del universo de una producción.
+                    A lo largo de estos 9 meses, se llevarán a cabo masterclasses con invitados especiales: directores, productores, cinefotógrafos, guionistas, entre otros, que complementarán y reforzarán el desarrollo y la experiencia de los alumnos.
+                    Hemos hecho una selección de los mejores cineastas y realizadores locales con la finalidad de ofrecer una visión real, fresca y cercana a las nuevas tendencias, quienes compartirán sus conocimientos a partir de su experiencia en decenas de producciones, en las cuales han logrado recibir premios, selecciones de festivales y reconocimiento como creadores.
+                    Creemos en la libertad creativa y la experimentación constante como los medios más efectivos para la creación artística. No tenemos límites, somos creados imparables.
+                </p>
             </div>
         </div>
         <h3>Áreas a cursar</h3>
@@ -21,47 +28,50 @@
             <div class="col-12 col-md-7">
                 <h4>Módulo 1</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>Lenguaje y apreciación cinematográfica (LAC)</li>
+                    <li>Guion 1</li>
+                    <li>Cinefotografía 1</li>
+                    <li>Producción (PREPRODUCCIÓN)</li>
+                    <li>Actuación</li>
                 </ul>
                 <h4>Módulo 2</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>Sonido Directo</li>
+                    <li>Guion 2</li>
+                    <li>Dirección 1</li>
+                    <li>Cinefotografía 2</li>
+                    <li>Producción 1 (PRODUCCIÓN)</li>
+                    <li>Montaje</li>
                 </ul>
                 <h4>Módulo 3</h4>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <li>Documental</li>
+                    <li>Producción 2 (POSTPRODUCCIÓN)</li>
+                    <li>Dirección 2</li>
+                    <li>Corrección de color</li>
+                    <li>Diseño sonoro</li>
                 </ul>
             </div>
             <div class="col-12 col-md-5">
                 <p>
-                    <span>INSCRIPCIÓN $4,400</span><br>
-                    <span>COSTO POR MÓDULO</span><br>
-                    <span>DESCUENTO MASTER</span><br>
-                    <span>CLASES OPCIONALES</span><br>
+                    <span><strong>INSCRIPCIÓN:</strong>  $350.00</span><br>
+                    <span><strong>COSTO POR MÓDULO:</strong> $2,200.00</span><br>
+                    <span><strong>DESCUENTO MASTER</strong></span><br>
+                    <span><strong>CLASES OPCIONALES:</strong> 60%</span><br>
                 </p>
-                <p>
-                    <span>REQUISITOS DE INSCRIPCIÓN:</span>
+                <p class="mt-5">
+                    <strong>REQUISITOS DE INSCRIPCIÓN:</strong>
                 </p>
-                <p>
-                    <span>lorem lorem</span><br>
-                    <span>lorem lorem</span><br>
-                    <span>lorem lorem</span><br>
-                    <span>lorem lorem</span><br>
-                    <span>lorem lorem</span><br>
-                    <span>lorem lorem</span><br>
-                </p>
+                <ul>
+                    <li>Acta de Nacimiento</li>
+                    <li>Identificación Oficial</li>
+                    <li>En caso de menores de edad:</li>
+                    <li>firma de padre o tutor</li>
+                    <li>Comprobante de Domicilio</li>
+                    <li>CURP</li>
+                    <li>Último grado de estudios</li>
+                    <li>Aplicar favorablemente Test de selección</li>
+                </ul>
             </div>
         </div>
     </div>
